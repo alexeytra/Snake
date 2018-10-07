857b3f4ebc612cb40b808a578f549cac35bc14d4
alex <alekceytr@mail.ru>
2018-10-07 19:49:58 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main() {
+        setTitle("Snake");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        Main main = new Main();
+
+    }
+}
